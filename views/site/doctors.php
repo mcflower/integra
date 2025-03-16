d7395eff159d86cd5c90f47349689313a361528d
Ilya Zelenskiy <greenfild@gmail.com>
2025-03-16 18:25:28 +0400

    vilavi убрал

--- views/site/doctors.php
+++ views/site/doctors.php
@@ -36,7 +36,7 @@ $r = array('да' => 'да', 'нет' => 'нет');
         <?= $form->field($model, 'q10')->textInput(['maxlength' => true, 'placeholder' => 'Ваш ответ'])?>
         <?= $form->field($model, 'q11')->radioList($r)?>
         <?//= $form->field($model, 'q12')->textInput(['maxlength' => true, 'placeholder' => 'Ваш ответ'])?>
-        <?= $form->field($model, 'q13')->radioList($r)?>
+        <?//= $form->field($model, 'q13')->radioList($r)?>
         <?= $form->field($model, 'policy')->checkbox([
             'label' => 'Подтверждаете согласие на обработку и хранение Персональных данных согласно Федерального закона "О персональных данных" от 27.07.2006 N 152-ФЗ , в т.ч. Вы выражаете согласие на совершение звонка на Ваш мобильный номер телефона и получение сообщений в мессенджеры в соответствии с ФЗ "О связи" от 07.07.2003 N 126-ФЗ.',
             'labelOptions' => [
